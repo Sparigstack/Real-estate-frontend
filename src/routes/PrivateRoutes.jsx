306b3f38136c2c39ed21718c98ed493cb3f489d0
RiyaSprigstack <riyadarji.sprigstack@outlook.com>
2024-11-29 18:14:04 +0530

    changes

--- src/routes/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet} from 'react-router-dom';
-import Cookies from 'js-cookie';
-import Login from '../pages/Signup-Login/Login';
-
-export default function PrivateRoutes() {
-    const token = Cookies.get('authToken'); 
-    return token ?  <Outlet /> : <Login/>  
-}
